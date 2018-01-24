085a3046ae7a81ef8941df652b1a9987e28aaef3
Emily Osowski <emosowski@gmail.com>
2018-01-24 08:48:58 -0600

    Todo list complete with datasource and delegate methods.

---
+++ 2Do2Day/TodoListViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  2Do2Day
+//
+//  Created by Emily on 1/23/18.
+//  Copyright © 2018 emilyosowski. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Code", "Laundry", "Cook lunch"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - Tableview Datasource Methods
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        //add/remove checkmark on click
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        //deselects row after click
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+
+}
+
